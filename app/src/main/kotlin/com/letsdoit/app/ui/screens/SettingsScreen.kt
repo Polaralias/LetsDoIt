ebdffea335f1987870e72fe651e7c938b7d75400
Polaralias <76123410+Polaralias@users.noreply.github.com>
2025-10-25 07:34:49 +0100

    Import RoundedCornerShape in settings screen

--- app/src/main/kotlin/com/letsdoit/app/ui/screens/SettingsScreen.kt
+++ app/src/main/kotlin/com/letsdoit/app/ui/screens/SettingsScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
