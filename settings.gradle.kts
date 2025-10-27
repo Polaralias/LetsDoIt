a8947ac6575d08b7bc15d138153b800e2adac4c9
Polaralias <76123410+Polaralias@users.noreply.github.com>
2025-10-27 22:06:55 +0000

    Remove plugin resolution override

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,22 +13,6 @@ pluginManagement {
         id("androidx.baselineprofile") version "1.2.4"
         id("androidx.benchmark") version "1.2.4"
     }
-    resolutionStrategy {
-        eachPlugin {
-            when (requested.id.id) {
-                "com.android.application", "com.android.test" ->
-                    useModule("com.android.tools.build:gradle:${requested.version}")
-                "org.jetbrains.kotlin.android", "org.jetbrains.kotlin.kapt" ->
-                    useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
-                "com.google.dagger.hilt.android" ->
-                    useModule("com.google.dagger:hilt-android-gradle-plugin:${requested.version}")
-                "androidx.baselineprofile" ->
-                    useModule("androidx.baselineprofile:androidx.baselineprofile.gradle.plugin:${requested.version}")
-                "androidx.benchmark" ->
-                    useModule("androidx.benchmark:benchmark-gradle-plugin:${requested.version}")
-            }
-        }
-    }
 }
 
 dependencyResolutionManagement {
